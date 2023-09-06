4400033310db074f5413961689dc9de17c70407b
unknown <kybk9a6@gmail.com>
2023-09-06 22:39:03 +0700

    UPDATE3

---
+++ THUCHIEN/js/script.js
@@ -0,0 +1,44 @@
+let currentStep = 1;
+const progressBar = document.getElementById('progress');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+function prevStep(step) {
+    if (step > 1) {
+        document.getElementById(`step${currentStep}`).style.display = 'none';
+        currentStep = step - 1;
+        document.getElementById(`step${currentStep}`).style.display = 'block';
+
+        const progressWidth = (currentStep - 1) * 33.33;
+        progressBar.style.width = `${progressWidth}%`;
+
+        updateButtons();
+    }
+}
+
+function nextStep(step) {
+    if (step < 3) {
+        document.getElementById(`step${currentStep}`).style.display = 'none';
+        currentStep = step + 1;
+        document.getElementById(`step${currentStep}`).style.display = 'block';
+
+        const progressWidth = (currentStep - 1) * 33.33;
+        progressBar.style.width = `${progressWidth}%`;
+
+        updateButtons();
+    }
+}
+
+function updateButtons() {
+    if (currentStep === 1) {
+        prevButton.style.display = 'none';
+    } else if (currentStep === 3) {
+        nextButton.style.display = 'none';
+    } else {
+        prevButton.style.display = 'block';
+        nextButton.style.display = 'block';
+    }
+}
+
+document.getElementById('step1').style.display = 'block';
+updateButtons();
